3a24b5c7970c16571ed2cbb9dc9f503c9a93d279
gregoire renaldo <gregoire.renaldo@gmail.com>
2025-01-15 20:59:41 +0100

    test

--- pipe1.py
+++ pipe1.py
@@ -3,6 +3,6 @@ from transformers import pipeline
 # sentiment analysis is a task
 classifier = pipeline("sentiment-analysis")
 
-res = classifier("hello world, nice to meet you.I have been waiting for you for a long time")
+res = classifier("hello world, nice to meet you.May I ask you something?")
 
 print(res)
