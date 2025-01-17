1975d85475c33a8b7a7f53cb042443286f889791
gregoire renaldo <gregoire.renaldo@gmail.com>
2025-01-17 11:51:53 +0100

    tokenizer

--- pipe1.py
+++ pipe1.py
@@ -1,4 +1,5 @@
 from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # sentiment analysis is a task
 classifier = pipeline("sentiment-analysis")
@@ -6,3 +7,29 @@ classifier = pipeline("sentiment-analysis")
 res = classifier("hello world, nice to meet you.May I ask you something?")
 
 print(res)
+
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+model =  AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+res = classifier("hello world, nice to meet you.May I ask you something?")
+
+print(res)
+
+sequence = "Using transformers is easy!"
+
+res = tokenizer(sequence)
+print(res)
+
+tokens = tokenizer.tokenize(sequence)
+print(tokens)
+
+ids = tokenizer.convert_tokens_to_ids(tokens)
+print(ids)
+
+decoded_string = tokenizer.decode(ids)
+print(decoded_string)
+
+
