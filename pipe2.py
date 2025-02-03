4c79dc7e22edf8dfdf2dd0c1561c5bf4996e53aa
gregoire renaldo <gregoire.renaldo@gmail.com>
2025-02-03 17:03:56 +0100

    .

--- pipe2.py
+++ pipe2.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 generator = pipeline("text-generation", model="distilgpt2") # model="gpt2" is optional
 
 res = generator(
-  "hello world, nice to meet you, let's",
+  "hello world, nice to meet you, let's go",
   max_length=50, 
   num_return_sequences=50)
 
